2026807e2806545e4c824d4c355ce89636a25141
iLeonard0 <137785767+iLeonard0@users.noreply.github.com>
2024-02-27 11:02:08 -0300

    atividade6.java

---
+++ atividade6.java
@@ -0,0 +1,16 @@
+// ATIVIDADE 6 - AJUSTE DE CODIGO
+
+public class Main {
+    public static void main(String[] args) {
+       
+        String numeroProduto = "ABC123"; 
+        double precoProduto = 25; 
+    
+        String descricaoProduto = "Fone de ouvido Bluetooth"; 
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);  
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
