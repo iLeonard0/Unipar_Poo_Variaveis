84cde94c2082d5f3a6b3db79841e556e9145bb77
iLeonard0 <137785767+iLeonard0@users.noreply.github.com>
2024-02-27 08:58:39 -0300

    atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,12 @@
+// ATIVIDADE 02 - Nomenclatura
+
+a) nomeProduto
+b) precoProduto 
+c) marcaDoProduto 
+d) quantidadeProduto 
+e) descricaoProduto 
+f) nomeProduto 
+g) numeroProduto 
+h) precoDoProduto 
+i) estoqueDisponivel 
+j) totalDeProdutos
