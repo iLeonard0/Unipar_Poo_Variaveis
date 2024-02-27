e9e1d6117d1f63adaf4b5fa85dbee7b1350cd6de
iLeonard0 <137785767+iLeonard0@users.noreply.github.com>
2024-02-27 09:03:39 -0300

    atividade4.java

---
+++ atividade4.java
@@ -0,0 +1,11 @@
+// ATIVIDADE 4 - EMPRESA
+
+public class ControleDeEstoque {
+    public static void main(String[] args) {
+        
+        String nomeDoProduto; 
+        String categoriaDoProduto; 
+        double precoUnitarioDoProduto; 
+        int quantidadeEmEstoqueDoProduto; 
+    }
+}
